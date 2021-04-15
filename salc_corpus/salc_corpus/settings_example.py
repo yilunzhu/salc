d0e79d91490bb32f52f9c0b61f20ec172f03d0a2
yilunzhu <allen.yl.zhu@gmail.com>
2021-04-15 23:01:27 +0800

    removing secret keys

--- salc_corpus/salc_corpus/settings_example.py
+++ salc_corpus/salc_corpus/settings_example.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n9-+x-$zx)wgjq+_f!u=)9$joks%8#x(j26%u!(d$h0pxnee6-'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

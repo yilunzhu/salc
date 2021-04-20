7a77de159439afb2f695dd0d0339076b912d51ed
Jessica Lin <32357445+jl908069@users.noreply.github.com>
2021-04-20 17:28:20 +0800

    Update admin.py

--- salc_corpus/corpus/admin.py
+++ salc_corpus/corpus/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from . import models
+from .models import Corpus
+
+class CorpusAdmin(admin.ModelAdmin):
+    list_display = ("sample", "sopi","sopib", "pvc","language", "task", "utterance",)
 
 admin.site.register(models.Corpus)

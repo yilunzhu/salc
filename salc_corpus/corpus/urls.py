c3a2c9d2a59a738dda87e67593898ea7a78711d8
Jessica Lin <32357445+jl908069@users.noreply.github.com>
2021-05-22 10:27:29 +0800

    Update urls.py

--- salc_corpus/corpus/urls.py
+++ salc_corpus/corpus/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
 from corpus import views
+from django.conf.urls import url
+from .views import HomePageView,SearchResultsView,SearchlangView,SearchtaskView
+
+from django.conf.urls import url #new
+
 
 urlpatterns = [
-    path('index/', views.corpus_index, name='corpus_index'),
+
+    path('search/', SearchResultsView.as_view(), name='search_results'),
+    path('search_lang/', SearchlangView.as_view(), name='search_lang_results'),
+    path('search_task/', SearchtaskView.as_view(), name='search_task_results'),
+    path('', HomePageView.as_view(), name='home'),
+
 ]
